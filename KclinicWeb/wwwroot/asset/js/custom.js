2a93baa0ff1346b102f7e4df55bf21ff91202615
hoang la nhu <lanhuhoang261198@gmail.com>
2023-08-24 08:43:44 +0700

    edit1

--- KclinicWeb/wwwroot/asset/js/custom.js
+++ KclinicWeb/wwwroot/asset/js/custom.js
@@ -43,3 +43,17 @@
     new WOW({ mobile: false }).init();
 
 })(jQuery);
+$(document).ready(function () {
+    var icon = $("#contact-icon");
+    var iconPosition = icon.position().top;
+
+    $(window).scroll(function () {
+        var scrollPosition = $(this).scrollTop();
+
+        if (scrollPosition > iconPosition) {
+            icon.addClass("fixed-icon");
+        } else {
+            icon.removeClass("fixed-icon");
+        }
+    });
+});
