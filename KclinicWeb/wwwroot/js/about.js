e4e0a7f365877a34bddd7f265a9830518e452c9d
hoang la nhu <lanhuhoang261198@gmail.com>
2023-08-28 14:32:15 +0700

    xoa nut delete

--- KclinicWeb/wwwroot/js/about.js
+++ KclinicWeb/wwwroot/js/about.js
@@ -19,8 +19,6 @@ function loadDataTable() {
                         <div class="w-75 btn-group" role="group">
                         <a href="/Admin/About/Upsert?id=${data}"
                         class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
-                        <a onClick=Delete('/Admin/About/Delete/${data}')
-                        class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
 					</div>
                         `
                 },
